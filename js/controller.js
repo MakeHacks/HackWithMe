8e0071d0f54a128188dacad089f9ce40d36a7be1
Armaiz Adenwala <armaiz.adenwala@gmail.com>
2015-11-15 13:18:04 -0800

    Added more people to the database

--- js/controller.js
+++ js/controller.js
@@ -19,7 +19,7 @@ angular.module('app').controller("MainController", function(){
 	vm.people = [
 		{
 			name: 'John Doe',
-			img: 'https://lh5.googleusercontent.com/-ZadaXoUTBfs/AAAAAAAAAAI/AAAAAAAAAGA/19US52OmBqc/photo.jpg',
+			img: 'https://cap.stanford.edu/profiles/viewImage?profileId=19142&type=square',
 			tags: [
 				'Javascript', 'HTML', 'CSS'
 			],
@@ -51,7 +51,7 @@ angular.module('app').controller("MainController", function(){
 
               {
 			name: 'Joseph Stone',
-			img: 'http://www.realtimearts.net/data/images/art/46/4640_profile_nilssonpolias.jpg',
+			img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7RhLIFhh_5aeM0nsZNNtp6uVbMiOEAY7oGwI-BnZlM4OetErL',
 			tags: [
 				'Ruby', 'Rails', 'Javascript', 'C++'
 			],
@@ -67,7 +67,7 @@ angular.module('app').controller("MainController", function(){
 
               {
 			name: 'Stephanie Blossom',
-			img: "http://www.lawyersweekly.com.au/images/LW_Media_Library/594partner-profile-pic-An.jpg",
+			img: "http://www.morganstanley.com/assets/images/people/tiles/audrey-choi-large.jpg",
 			tags: [
 				'Javascript', 'JQuery', 'HTML'
 			],
@@ -145,7 +145,7 @@ angular.module('app').controller("MainController", function(){
 
               {
 			name: 'James Key',
-			img: "http://www.beatpennystocks.com/wp-content/uploads/2013/06/profile_face_small_normal.jpg",
+			img: "http://www.momochicago.org/wp-content/uploads/2013/05/Seb-profile-square_normal.jpg",
 			tags: [
 				'Phonegap', 'HTML', 'CSS', 'Android'
 			],
@@ -158,7 +158,51 @@ angular.module('app').controller("MainController", function(){
             	'MakeHacks', 'TeenTechSF'
             ]
 		},
-
+		              {
+			name: 'Tom Tucker',
+			img: "http://www.american.edu/uploads/profiles/large/nisbet_portrait_profile.jpg",
+			tags: [
+				'Phonegap', 'HTML', 'CSS', 'Android'
+			],
+			github: 'ttuck1242',
+          	email: 'TTucker@aol.com',
+         	phone: '555-555-5555',
+          	distance: 16,
+          	address: 'Walnut Creek',
+          	hackathons: [
+            	'MakeHacks', 'TeenTechSF', 'HackingEDU'
+            ]
+		},
+		              {
+			name: 'Raj Patel',
+			img: "https://pbs.twimg.com/profile_images/1215190827/DSC_3900_400x400.jpg",
+			tags: [
+				'Python', 'Django', 'Html', 'Javascript'
+			],
+			github: 'rPatel2431231',
+          	email: 'rajPatel2984@yahoo.com',
+         	phone: '555-555-5555',
+          	distance: 26,
+          	address: 'Palo Alto',
+          	hackathons: [
+            	'TeenTechSF'
+            ]
+		},
+		              {
+			name: 'Cindy Lopez',
+			img: "http://1.bp.blogspot.com/-WVNJpabEzRA/U7Vx019EwHI/AAAAAAAAA38/nft9gcYlnFs/s1600/Zulimar-headshot1.jpg",
+			tags: [
+				'Java', 'IOS', 'Swift', 'Android'
+			],
+			github: 'cind242',
+          	email: 'cind242@gmail.com',
+         	phone: '555-555-5555',
+          	distance: 46,
+          	address: 'Davis',
+          	hackathons: [
+            	'HackingEDU', 'TeenTechSF'
+            ]
+		},
 
 
 	];
