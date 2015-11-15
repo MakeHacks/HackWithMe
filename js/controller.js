42eddead66ee9af83b287e76f7c0607bbcaaa5ce
Vishal Sethi <vishal.kumar.sethi1@gmail.com>
2015-11-14 16:13:27 -0800

    added profile images

--- js/controller.js
+++ js/controller.js
@@ -4,7 +4,7 @@ angular.module('app').controller("MainController", function(){
 	vm.people = [
 		{
 			name: 'Lorem Ipsum',
-			img: '',
+			img: 'https://lh5.googleusercontent.com/-ZadaXoUTBfs/AAAAAAAAAAI/AAAAAAAAAGA/19US52OmBqc/photo.jpg',
 			tags: [
 				'Javascript', 'Html', 'CSS'
 			],
@@ -12,7 +12,7 @@ angular.module('app').controller("MainController", function(){
 		},
 		{
 			name: 'Ipsum Dolor',
-			img: '',
+			img: 'https://upload.wikimedia.org/wikipedia/en/7/70/Shawn_Tok_Profile.jpg',
 			tags: [
 				'Html', 'CSS'
 			],
