dc05a6ebff8ca1f5f94ef5ddab515fc5fb4fb4a9
Vishal Sethi <vishal.kumar.sethi1@gmail.com>
2015-11-14 19:12:08 -0800

    Added Data

--- js/controller.js
+++ js/controller.js
@@ -3,20 +3,113 @@ angular.module('app').controller("MainController", function(){
 	vm.searchInput = '';
 	vm.people = [
 		{
-			name: 'Lorem Ipsum',
+			name: 'John Doe',
 			img: 'https://lh5.googleusercontent.com/-ZadaXoUTBfs/AAAAAAAAAAI/AAAAAAAAAGA/19US52OmBqc/photo.jpg',
 			tags: [
 				'Javascript', 'Html', 'CSS'
 			],
-			github: ''
+			github: '',
+                              email: '',
+                              phone: '',
+                              city: '',
 		},
 		{
-			name: 'Ipsum Dolor',
+			name: 'Bob Smith',
 			img: 'https://upload.wikimedia.org/wikipedia/en/7/70/Shawn_Tok_Profile.jpg',
 			tags: [
 				'Html', 'CSS'
 			],
 			github: ''
-		}
+		},
+              {
+			name: 'Joseph Stone',
+			img: 'https://lh5.googleusercontent.com/-ZadaXoUTBfs/AAAAAAAAAAI/AAAAAAAAAGA/19US52OmBqc/photo.jpg',
+			tags: [
+				'Javascript', 'Html', 'CSS'
+			],
+			github: '',
+                              email: '',
+                              phone: '',
+                              city: '',
+		},
+              {
+			name: 'Lorem Ipsum',
+			img: 'https://lh5.googleusercontent.com/-ZadaXoUTBfs/AAAAAAAAAAI/AAAAAAAAAGA/19US52OmBqc/photo.jpg',
+			tags: [
+				'Javascript', 'Html', 'CSS'
+			],
+			github: '',
+                              email: '',
+                              phone: '',
+                              city: '',
+		},
+              {
+			name: 'Lorem Ipsum',
+			img: 'https://lh5.googleusercontent.com/-ZadaXoUTBfs/AAAAAAAAAAI/AAAAAAAAAGA/19US52OmBqc/photo.jpg',
+			tags: [
+				'Javascript', 'Html', 'CSS'
+			],
+			github: '',
+                              email: '',
+                              phone: '',
+                              city: '',
+		},
+              {
+			name: 'Lorem Ipsum',
+			img: 'https://lh5.googleusercontent.com/-ZadaXoUTBfs/AAAAAAAAAAI/AAAAAAAAAGA/19US52OmBqc/photo.jpg',
+			tags: [
+				'Javascript', 'Html', 'CSS'
+			],
+			github: '',
+                              email: '',
+                              phone: '',
+                              city: '',
+		},
+              {
+			name: 'Lorem Ipsum',
+			img: 'https://lh5.googleusercontent.com/-ZadaXoUTBfs/AAAAAAAAAAI/AAAAAAAAAGA/19US52OmBqc/photo.jpg',
+			tags: [
+				'Javascript', 'Html', 'CSS'
+			],
+			github: '',
+                              email: '',
+                              phone: '',
+                              city: '',
+		},
+              {
+			name: 'Lorem Ipsum',
+			img: 'https://lh5.googleusercontent.com/-ZadaXoUTBfs/AAAAAAAAAAI/AAAAAAAAAGA/19US52OmBqc/photo.jpg',
+			tags: [
+				'Javascript', 'Html', 'CSS'
+			],
+			github: '',
+                              email: '',
+                              phone: '',
+                              city: '',
+		},
+              {
+			name: 'Lorem Ipsum',
+			img: 'https://lh5.googleusercontent.com/-ZadaXoUTBfs/AAAAAAAAAAI/AAAAAAAAAGA/19US52OmBqc/photo.jpg',
+			tags: [
+				'Javascript', 'Html', 'CSS'
+			],
+			github: '',
+                              email: '',
+                              phone: '',
+                              city: '',
+		},
+              {
+			name: 'Lorem Ipsum',
+			img: 'https://lh5.googleusercontent.com/-ZadaXoUTBfs/AAAAAAAAAAI/AAAAAAAAAGA/19US52OmBqc/photo.jpg',
+			tags: [
+				'Javascript', 'Html', 'CSS'
+			],
+			github: '',
+                              email: '',
+                              phone: '',
+                              city: '',
+		},
+              
+              
 	];
 });
